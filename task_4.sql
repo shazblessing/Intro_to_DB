02fb9044bee5435b33863af88cefbc8698c0a006
Wanjiku <wanjikusharon59@gmail.com>
2024-10-20 22:28:14 +0300

    book

--- task_4.sql
+++ task_4.sql
@@ -9,5 +9,5 @@ SELECT
 FROM 
     INFORMATION_SCHEMA.COLUMNS 
 WHERE 
-    TABLE_SCHEMA = DATABASE() 
-    AND TABLE_NAME = 'Books';
+    TABLE_SCHEMA = alx_book_store() 
+    AND TABLE_NAME = 'books';
