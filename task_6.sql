dae1ff5cf20d4b3dd59f9667f8708132106e66a3
Wanjiku <wanjikusharon59@gmail.com>
2024-10-21 12:53:25 +0300

    task_6

--- task_6.sql
+++ task_6.sql
@@ -1,4 +1,3 @@
--- Insert multiple rows into the Customers table
 INSERT INTO customers (customer_id, customer_name, email, address) 
 VALUES (2, 'Blessing Malik', 'bmalik@sandtech.com', "124 Happiness Ave."),
        (3, 'Obed Ehoneah', 'eobed@sandtech.com', '125 Happiness Ave.'),
