eef049d3fdce13977d56894d2e1b81e874f626e8
Wanjiku <wanjikusharon59@gmail.com>
2024-10-21 12:40:19 +0300

    TASK 4

--- task_4.sql
+++ task_4.sql
@@ -1,13 +1,5 @@
 -- Query to get the full description of the 'books' table
-SELECT 
-    COLUMN_NAME, 
-    DATA_TYPE, 
-    IS_NULLABLE, 
-    COLUMN_KEY, 
-    COLUMN_DEFAULT, 
-    EXTRA 
-FROM 
-    INFORMATION_SCHEMA.COLUMNS 
-WHERE 
-    TABLE_SCHEMA = "alx_book_store"
-    AND TABLE_NAME = 'books';
+SELECT COLUMN_NAME, COLUMN_TYPE, IS_NULLABLE, COLUMN_KEY, COLUMN_DEFAULT, EXTRA
+FROM INFORMATION_SCHEMA.COLUMNS 
+WHERE TABLE_SCHEMA = 'alx_book_store'AND TABLE_NAME = 'Books';
+ 
